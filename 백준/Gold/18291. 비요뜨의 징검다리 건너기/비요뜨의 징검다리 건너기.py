19a2a200e1c5d7d5df5acc36d7a67b10ca8393d2
trolgod0914 <150028805+trolgod0914@users.noreply.github.com>
2023-11-08 01:36:16 +0900

    [Gold V] Title: 비요뜨의 징검다리 건너기, Time: 148 ms, Memory: 113220 KB -BaekjoonHub

---
+++ 백준/Gold/18291. 비요뜨의 징검다리 건너기/비요뜨의 징검다리 건너기.py
@@ -0,0 +1,22 @@
+import sys
+input = sys.stdin.readline
+print = sys.stdout.write
+def Supply(N, M, L):
+    if M == 0:
+        return 1
+    if M == 1:
+        return N % L
+    if M == 2:
+        return (N * N) % L
+    else:
+        if M % 2 == 0:
+            return (Supply(N, M//2, L) ** 2) % L
+        else:
+            return ((Supply(N, M//2, L) ** 2) * N) % L
+
+for i in range(int(input())):
+    N = int(input().rstrip())
+    if N == 1:
+        print('1\n')
+    else:
+        print(str(Supply(2, N-2, 10**9+7))+'\n')
