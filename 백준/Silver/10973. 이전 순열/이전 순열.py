e5f8486fa08ab892120b1b30bbc85b92f8d6285f
trolgod0914 <150028805+trolgod0914@users.noreply.github.com>
2023-12-07 21:21:30 +0900

    [Silver III] Title: 이전 순열, Time: 128 ms, Memory: 110840 KB -BaekjoonHub

---
+++ 백준/Silver/10973. 이전 순열/이전 순열.py
@@ -0,0 +1,28 @@
+N = int(input())
+Array = list(map(int, input().split()))
+
+if N == 1:
+    print(-1)
+    exit()
+
+key = []
+for i in range(N-1):
+    if Array[i] > Array[i+1]:
+        key.append(i)
+
+if key:
+    K = key[-1]
+else:
+    print(-1)
+    exit()
+
+value = Array[K]
+for j in range(K+1, N):
+    if Array[j] < value:
+        idx = j
+
+Array[K], Array[idx] = Array[idx], Array[K]
+Left, Right = Array[:K+1], Array[K+1:]
+Right.sort(reverse=True)
+Answer = Left + Right
+print(*Answer)
