56824d3e182292a7feb5fc3c3938e00f62a4a2f3
trolgod0914 <150028805+trolgod0914@users.noreply.github.com>
2023-12-09 08:22:55 +0900

    [Bronze IV] Title: Pizza, Time: 112 ms, Memory: 108080 KB -BaekjoonHub

---
+++ 백준/Bronze/26566. Pizza/Pizza.py
@@ -0,0 +1,9 @@
+from math import *
+for _ in range(int(input())):
+    A1, P1 = map(int, input().split())
+    R1, P2 = map(int, input().split())
+    W, P = R1**2*pi/P2, A1/P1
+    if W > P:
+        print('Whole pizza')
+    else:
+        print('Slice of pizza')
