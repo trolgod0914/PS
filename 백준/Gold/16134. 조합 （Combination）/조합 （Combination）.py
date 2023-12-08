4dc81d003865b2478082b1e9912bc18b80637615
trolgod0914 <150028805+trolgod0914@users.noreply.github.com>
2023-12-08 09:59:34 +0900

    [Gold I] Title: 조합 (Combination), Time: 464 ms, Memory: 33240 KB, Score: 100 point -BaekjoonHub

--- 백준/Gold/16134. 조합 （Combination）/조합 （Combination）.py
+++ 백준/Gold/16134. 조합 （Combination）/조합 （Combination）.py
@@ -1,13 +1,25 @@
-import operator as op
-from functools import reduce
-N, K = map(int, input().split())
-M = int(1e9+7)
-def Combination(n, r):
-    if n == 0 and r == 0:
-        return 1
-    r = min(r, n-r)
-    numerator = reduce(op.mul, range(n, n-r, -1), 1)
-    denominator = reduce(op.mul, range(1, r+1), 1)
-    return numerator // denominator
+import sys, math
+input = sys.stdin.readline
+print = sys.stdout.write
+sys.setrecursionlimit(1000000)
 
-print(Combination(N, K)%M)
+N, R = map(int, input().split())
+Mod = int(1e9+7)
+
+def power(a, n):
+    if n == 0:
+        return 1
+    x = power(a, n//2)
+    if n % 2 == 0:
+        return (x * x) % Mod
+    else:
+        return (x * x * a) % Mod
+
+N1, N2 = 1, 1
+K = N-R
+for i in range(1, R+1):
+    N1 *= (K+i)
+    N2 *= i
+    N1, N2 = N1%Mod, N2%Mod
+
+print(str(power(N2, Mod-2) * N1 % Mod) + '\n')
